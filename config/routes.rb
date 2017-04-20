6b83d0b72c59cbe60d1f7be31dc623406e8b0ab1
jarrar farman ali <jarrar_farman@hotmail.com>
2017-04-20 13:04:28 +0500

    Update thusday

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+ 
+  root 'welcome#index'
+end
+
+Rails.application.routes.draw do
+ 
+  resources :articles
+ 
+  root 'welcome#index'
+end
